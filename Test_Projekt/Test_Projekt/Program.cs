7b8ae111fd886883d81fc5d191a35880e19a015d
lennartbruns <71816482+lennartbruns@users.noreply.github.com>
2020-10-23 15:26:29 +0200

    Update Program.cs

--- Test_Projekt/Test_Projekt/Program.cs
+++ Test_Projekt/Test_Projekt/Program.cs
@@ -6,7 +6,112 @@ namespace Test_Projekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Double m;       //m = Modul
+            Double p;       //p = Teilung
+            Double d;       //d = Teilkreisdurchmesser
+            Double da;      //da = Kopfkreisdurchmesser
+            Double df;      //df = Fußkreisdurchmesser
+            Double z;       //z = Zähnezahl
+            Double h;       //h = Zahnhöhe   
+            Double ha;      //ha = Zahnkopfhöhe
+            Double hf;      //hf = Zahnfußhöhe
+            Double c;       //c = Kopfspiel
+            Double a;       //a = Achsenabstand
+            Double alpha;   //Nachnorm 20 
+            Double dg;      //dg = Grundkreisdurchmesser
+
+            alpha = 20; //Zum testen
+            c = 0.167; //vorgabe
+
+            int Eingabe = 0; //Eingabe als Int deklarieren
+
+            do
+            {
+                Console.WriteLine("\n\t\t\tZahnradberechnungsprogramm von Gruppe H");
+                Console.WriteLine("\n\t\tWollen sie die Modul und Teilkreisdurchmesser vorgeben press 1");
+                Console.WriteLine("\n\t\tWollen sie die Modul und Zähnezahl vorgeben press 2");
+                Console.WriteLine("\n\t\tWollen sie die Zähnezahl und Teilkreisdurchmesser vorgeben press 3");
+                Console.WriteLine("\n\t\t\tund bestätigen sie ihre Eingabe mit Enter");
+
+                Eingabe = Convert.ToInt32(Console.ReadLine());    // Eingabe und convert
+
+                if (Eingabe >= 4 || Eingabe <= 0) //Fehlermeldung
+                {
+                    Console.Clear();
+                    Console.WriteLine("\n\t\t\t\tFehler! Falsche Eingabe!\n\t\t\t\t Eingabe Wiederholen!\n");
+
+                }
+
+
+            }
+
+            while (Eingabe >= 4 || Eingabe <= 0); //Wenn Eingabe Falsch: Neustart
+
+
+            if (Eingabe == 1)
+            {
+                Console.WriteLine("Bitte geben sie das Modul ein.");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie den Teilkreisdurchmesser in mm an");
+                d = Convert.ToDouble(Console.ReadLine());
+                z = d / m;
+
+                berechnung();
+                ausgabe();
+
+            }
+            else if (Eingabe == 2)
+            {
+                Console.WriteLine("Bitte geben sie das Modul ein.");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie die Zähnezahl an:");
+                z = Convert.ToDouble(Console.ReadLine());
+                d = m * z;
+
+                berechnung();
+                ausgabe();
+
+            }
+            else if (Eingabe == 3)
+            {
+                Console.WriteLine("Bitte geben sie das Teilkreisdurchmesser in mm ein.");
+                d = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie die Zähnezahl an:");
+                z = Convert.ToDouble(Console.ReadLine());
+                m = d / z;
+
+                berechnung();
+                ausgabe();
+
+            }
+
+            //Unterprogramme
+            void ausgabe()
+            {
+                Console.WriteLine(Eingabe);
+                Console.WriteLine("Zähnezahl: " + z);
+                Console.WriteLine("Teilkreisdurchmesser: " + d);
+                Console.WriteLine("Modul: " + m);
+                Console.WriteLine("Teilung: " + p);
+                Console.WriteLine("Fußkreisdurchmesser: " + df);
+                Console.WriteLine("Zahnhöhe: " + h);
+                Console.WriteLine("Grundkreisdurchmesser: " + dg);
+            }
+
+            void berechnung()
+            {
+                //Zahnhöhe
+                h = 2 * m + c;
+                //Teilung
+                //Pi genauer eingeben?
+                p = Math.PI * m;
+                //Fußkreisdurchmesser
+                df = d - (2 * (m + c));
+                //Grundkreisdurchmesser
+                alpha = Math.PI / 180 * alpha;
+                dg = d * Math.Cos(alpha);
+            }
+            Console.ReadKey();
         }
     }
 }
