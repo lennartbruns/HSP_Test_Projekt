26d9836c8383c87431b675c0c4bbf96707478729
lennartbruns <71816482+lennartbruns@users.noreply.github.com>
2020-10-24 20:08:56 +0200

    Delete Test Projekt.AssemblyInfo.cs

--- Test Projekt/Test Projekt/obj/Debug/netcoreapp3.1/Test Projekt.AssemblyInfo.cs
+++ Test Projekt/Test Projekt/obj/Debug/netcoreapp3.1/Test Projekt.AssemblyInfo.cs
@@ -15,7 +15,6 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Test Projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
